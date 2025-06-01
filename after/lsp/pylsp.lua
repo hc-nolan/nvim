3b66f7b7ed04110bd87484c05e6138be50efaaa8
hnolan <hayden@hnolan.ca>
2025-06-01 01:01:11 -0400

    add mypy

--- after/lsp/pylsp.lua
+++ after/lsp/pylsp.lua
@@ -1,14 +1,31 @@
+-- https://jdhao.github.io/2023/07/22/neovim-pylsp-setup/
+local venv_path = os.getenv('VIRTUAL_ENV')
+local py_path = nil
+-- decide which python executable to use for mypy
+if venv_path ~= nil then
+  py_path = venv_path .. "/bin/python3"
+else
+  py_path = vim.g.python3_host_prog
+end
+
 -- pylsp
 return {
-	settings = {
-		pylsp = {
-			plugins = {
-				pyflakes = { enabled = false },
-				pylint = { enabled = false },
-				pycodestyle = {
-					maxLineLength = 100,
-				},
-			},
-		},
-	},
+  settings = {
+    pylsp = {
+      plugins = {
+        pyls_isort = { enabled = true },
+        pylsp_mypy = {
+          enabled = true,
+          overrides = { "--python-executable", py_path, true },
+          report_progress = true,
+          live_mode = false,
+        },
+        pyflakes = { enabled = false },
+        pylint = { enabled = false },
+        pycodestyle = {
+          maxLineLength = 100,
+        },
+      },
+    },
+  },
 }
