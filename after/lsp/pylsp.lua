ac6cd974e6a1c17d77ec7d04b56451a4723a15e4
hnolan <hayden@hnolan.ca>
2025-07-08 01:09:46 -0400

    update

--- after/lsp/pylsp.lua
+++ after/lsp/pylsp.lua
@@ -1,31 +1,38 @@
 -- https://jdhao.github.io/2023/07/22/neovim-pylsp-setup/
-local venv_path = os.getenv('VIRTUAL_ENV')
+local venv_path = os.getenv("VIRTUAL_ENV")
 local py_path = nil
 -- decide which python executable to use for mypy
 if venv_path ~= nil then
-  py_path = venv_path .. "/bin/python3"
+	py_path = venv_path .. "/bin/python3"
 else
-  py_path = vim.g.python3_host_prog
+	py_path = vim.g.python3_host_prog
 end
 
 -- pylsp
 return {
-  settings = {
-    pylsp = {
-      plugins = {
-        pyls_isort = { enabled = true },
-        pylsp_mypy = {
-          enabled = true,
-          overrides = { "--python-executable", py_path, true },
-          report_progress = true,
-          live_mode = false,
-        },
-        pyflakes = { enabled = false },
-        pylint = { enabled = false },
-        pycodestyle = {
-          maxLineLength = 100,
-        },
-      },
-    },
-  },
+	settings = {
+		pylsp = {
+			plugins = {
+				pyls_isort = { enabled = true },
+				pylsp_mypy = {
+					enabled = true,
+					overrides = { "--python-executable", py_path, true },
+					report_progress = true,
+					live_mode = false,
+				},
+				pyflakes = { enabled = false },
+				--[[ pylint = { enabled = false }, ]]
+				pylint = {
+					enabled = true,
+					args = {
+						"--disable=all",
+						"--enable=design",
+					},
+				},
+				pycodestyle = {
+					maxLineLength = 100,
+				},
+			},
+		},
+	},
 }
--- init.lua
+++ init.lua
@@ -13,42 +13,43 @@ vim.cmd([[colorscheme gruvbox-baby]])
 
 -- lsp --
 vim.lsp.config("*", {
-  capabilities = vim.lsp.protocol.make_client_capabilities(),
+	capabilities = vim.lsp.protocol.make_client_capabilities(),
 })
 require("mason").setup() -- make sure mason is initialized
 require("mason-lspconfig").setup({
-  ensure_installed = {
-    "lua_ls",
-    "ruff",
-    "pylsp",
-    "gopls",
-    "jinja_lsp",
-    "rust_analyzer",
-    "zls",
-    "html",
-    "cssls",
-    "css_variables",
-    "ansiblels",
-    "yamlls",
-    "bashls",
-    "dockerls",
-    "docker_compose_language_service",
-  },
+	ensure_installed = {
+		"clangd",
+		"lua_ls",
+		"ruff",
+		"pylsp",
+		"gopls",
+		"jinja_lsp",
+		"rust_analyzer",
+		"zls",
+		"html",
+		"cssls",
+		"css_variables",
+		"ansiblels",
+		"yamlls",
+		"bashls",
+		"dockerls",
+		"docker_compose_language_service",
+	},
 })
 vim.filetype.add({
-  filename = {
-    ["docker-compose.yml"] = "yaml.docker-compose",
-    ["docker-compose.yaml"] = "yaml.docker-compose",
-    ["compose.yml"] = "yaml.docker-compose",
-    ["compose.yaml"] = "yaml.docker-compose",
-  },
-  -- extension = {
-  --    note: jinja2 not currently working
-  -- 		html = "jinja",
-  -- 		jinja = "jinja",
-  -- 		jinja2 = "jinja",
-  -- 		j2 = "jinja",
-  -- 	},
+	filename = {
+		["docker-compose.yml"] = "yaml.docker-compose",
+		["docker-compose.yaml"] = "yaml.docker-compose",
+		["compose.yml"] = "yaml.docker-compose",
+		["compose.yaml"] = "yaml.docker-compose",
+	},
+	-- extension = {
+	--    note: jinja2 not currently working
+	-- 		html = "jinja",
+	-- 		jinja = "jinja",
+	-- 		jinja2 = "jinja",
+	-- 		j2 = "jinja",
+	-- 	},
 })
 -- local capabilities = vim.lsp.protocol.make_client_capabilities()
 -- local lspconfig = require("lspconfig")
