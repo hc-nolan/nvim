9c390fdadc8f657ecd0417f9d057b8e3a97962c9
hnolan <hayden@hnolan.ca>
2026-04-24 12:07:35 -0400

    Add obsidian.nvim

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -22,4 +22,13 @@ require("obsidian").setup({
 		date_format = "%Y-%m-%d",
 		time_format = "%H:%M",
 	},
+	picker = {
+		name = "telescope.nvim",
+		note_mappings = {
+			-- Create a new note from your query.
+			new = "<C-x>",
+			-- Insert a link to the selected note.
+			insert_link = "<C-l>",
+		},
+	},
 })
