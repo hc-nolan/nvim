4010cd7a37e40560937024b3002d7df154abd08a
hnolan <hayden@hnolan.ca>
2026-05-09 20:43:43 -0400

    add toggleterm and zeal

---
+++ lua/plugins/zeal.lua
@@ -0,0 +1,40 @@
+vim.pack.add({
+	-- { src = "https://github.com/paradoxical-dev/zeal.nvim" },
+	{ src = "https://github.com/hc-nolan/zeal.nvim" },
+})
+require("zeal").setup({
+	docsets_path = vim.fn.expand("~/.local/share/Zeal/Zeal/docsets"), -- zeal docset locations
+	browser = "w3m", -- can be any terminal browser
+	split = "vsplit", -- used when use_toggleterm = false
+	use_toggleterm = false,
+	-- toggleterm specifc options
+	-- see https://github.com/akinsho/toggleterm.nvim/tree/main
+	toggleterm = {
+		direction = "vertical",
+		split_size = vim.o.columns * 0.5, -- when direction != float
+	},
+	picker = {
+		type = "default", -- default | snacks
+		-- snacks picker specific options.
+		-- see https://github.com/folke/snacks.nvim/blob/main/docs/picker.md
+		snacks = {
+			layout = "default",
+		},
+	},
+	ft_map = {
+		lua = { "lua_5.5" },
+		python = { "python_3" },
+	},
+})
+
+vim.keymap.set({ "n", "v" }, "<leader>zz", function()
+	local zeal = require("zeal")
+	local bufnr = vim.api.nvim_get_current_buf()
+	local ft = vim.bo[bufnr].filetype
+	local settings = zeal.config
+	if settings.ft_map[ft] then
+		zeal.search_ft()
+	else
+		zeal.search()
+	end
+end, { desc = "Zeal: Search" })
