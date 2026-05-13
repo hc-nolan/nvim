18a6502744b0cbb915faf234b39e13fc879b27d3
hnolan <hayden@hnolan.ca>
2026-05-13 16:04:56 -0400

    update toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,7 +1,7 @@
 vim.pack.add({
 	{ src = "https://github.com/akinsho/toggleterm.nvim" },
 })
-require("toggleterm").setup({})
+require("toggleterm").setup({ size = 80 })
 
-vim.keymap.set({ "n", "v" }, "<leader>tt", ":ToggleTerm<CR>", { desc = "Toggleterm: Toggle" })
+vim.keymap.set({ "n", "v" }, "<leader>tt", ":ToggleTerm direction='vertical'<CR>", { desc = "Toggleterm: Toggle" })
 vim.keymap.set({ "n", "v" }, "<leader>tn", ":TermNew<CR>", { desc = "Toggleterm: New term" })
