c7a005443e9e786159e38e26aa2a4a2dd195f4a4
hnolan <hayden@hnolan.ca>
2026-05-21 21:15:30 -0400

    update hop

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,10 +1,9 @@
 vim.pack.add({
-  { src = 'https://github.com/smoka7/hop.nvim' },
+	{ src = "https://github.com/smoka7/hop.nvim" },
 })
-require('hop').setup({
-  opts = {
-      keys = 'etovxqpdygfblzhckisuran'
-  },
+require("hop").setup({
+	opts = {
+		keys = "etovxqpdygfblzhckisuran",
+	},
 })
-vim.keymap.set({ "n", "v" }, "<leader>q", "<cmd>HopWord<cr>", { desc = "Hop to word" })
-
+vim.keymap.set({ "n", "v" }, "<leader>h", "<cmd>HopWord<cr>", { desc = "Hop to word" })
