de45fe37fa805bdbe6a6f15dcec3b3289da8a50b
hnolan <hayden@hnolan.ca>
2026-05-21 21:15:18 -0400

    remove precognition

--- lua/plugins/precognition.lua
+++
@@ -1,3 +0,0 @@
-vim.pack.add({ { src = "https://github.com/tris203/precognition.nvim" } })
-require("precognition").setup()
-vim.keymap.set({ "n", "v" }, "<leader>P", ":Precognition toggle<CR>", { desc = "Precognition: toggle" })
