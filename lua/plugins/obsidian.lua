b5237d0dfb561bed8ca2c18a81af2d0d26d3091a
hnolan <hayden@hnolan.ca>
2026-05-21 21:15:24 -0400

    remove obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,34 +1,35 @@
-vim.pack.add({
-	{ src = "https://github.com/nvim-lua/plenary.nvim" },
-	{ src = "https://github.com/hrsh7th/nvim-cmp" },
-	{
-		src = "https://github.com/epwalsh/obsidian.nvim",
-	},
-})
-
-require("obsidian").setup({
-	workspaces = {
-		{
-			name = "vault",
-			path = "~/repos/work",
-		},
-	},
-	completion = {
-		nvim_cmp = true,
-		min_chars = 2,
-	},
-	templates = {
-		folder = "_templates",
-		date_format = "%Y-%m-%d",
-		time_format = "%H:%M",
-	},
-	picker = {
-		name = "telescope.nvim",
-		note_mappings = {
-			-- Create a new note from your query.
-			new = "<C-x>",
-			-- Insert a link to the selected note.
-			insert_link = "<C-l>",
-		},
-	},
-})
+return
+-- vim.pack.add({
+-- 	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+-- 	{ src = "https://github.com/hrsh7th/nvim-cmp" },
+-- 	{
+-- 		src = "https://github.com/epwalsh/obsidian.nvim",
+-- 	},
+-- })
+--
+-- require("obsidian").setup({
+-- 	workspaces = {
+-- 		{
+-- 			name = "vault",
+-- 			path = "~/repos/work",
+-- 		},
+-- 	},
+-- 	completion = {
+-- 		nvim_cmp = true,
+-- 		min_chars = 2,
+-- 	},
+-- 	templates = {
+-- 		folder = "_templates",
+-- 		date_format = "%Y-%m-%d",
+-- 		time_format = "%H:%M",
+-- 	},
+-- 	picker = {
+-- 		name = "telescope.nvim",
+-- 		note_mappings = {
+-- 			-- Create a new note from your query.
+-- 			new = "<C-x>",
+-- 			-- Insert a link to the selected note.
+-- 			insert_link = "<C-l>",
+-- 		},
+-- 	},
+-- })
